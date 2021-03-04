f62502ff3c1959b37232728af56545bed1f1fcaf
赵增君 <32793431+Junking1@users.noreply.github.com>
2021-03-04 14:09:01 +0800

    Add files via upload

---
+++ CNN3.py
@@ -0,0 +1,156 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import os
+import torch.utils.data as Data
+import torchvision
+
+
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name())
+
+Epoch = 50
+Batch_Size = 1024
+LR = 5e-3
+Restore_net = 0  #  0: not restore; 1:restore net; 2:restore params;
+Freeze = False
+Gamma = 0.95
+
+# define Dataload class
+class LoadData():
+    def dataload(self, root='./mnist', train=True, download=True):
+        train_sets = torchvision.datasets.MNIST(
+            root=root,
+            train=train,  #if True: trainsets, False: testsets;
+            transform=torchvision.transforms.ToTensor(),  # [0-255] to [0-1]
+            download=download
+        )
+        train_data = Data.DataLoader(dataset=train_sets, batch_size=Batch_Size, shuffle=True, num_workers=0)
+        return train_data
+
+
+# define network
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 24, 3, padding=1),  # (1,28,28)
+            nn.ReLU(),
+            nn.Conv2d(24, 24, 3, padding=1),  # (24,28,28)
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # (24,14,14)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(24, 48, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(48, 48, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)        #(48,7,7)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(48, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)  # (48,3,3)
+
+        )
+        self.Fc = nn.Linear(64 * 3 * 3, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(x.size(0), -1)
+        x = self.Fc(x)
+        return x
+
+
+if Restore_net == 1:
+    net = torch.load('net.pkl')
+elif Restore_net == 2:
+    net = CNN()
+    net.load_state_dict(torch.load('net_params.pkl'))
+else:
+    net = CNN()
+
+net = net.cuda(0)
+
+print(net)
+
+# define optimizer
+optimizer = torch.optim.Adam(params=net.parameters(), lr=LR)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=1, gamma=Gamma)
+
+# define loss function
+loss_func = nn.CrossEntropyLoss()
+
+# load traindata and test data
+train_data = LoadData().dataload(root='./mnist', train=True, download=True)
+test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+with torch.no_grad():
+    test_x = Variable(torch.unsqueeze(test_data.test_data, dim=1)).type(torch.FloatTensor)/255.
+    test_y = test_data.test_labels
+
+# train and Test
+for epoch in range(Epoch):
+
+    # Train
+    ACC = 0
+    Loss = 0
+    if Freeze:
+        break
+
+    for step, (batch_x, batch_y) in enumerate(train_data):
+        x = Variable(batch_x).cuda(0)
+        y = Variable(batch_y).cuda(0)
+        output = net(x)
+        loss = loss_func(output, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        with torch.no_grad():
+            equal = torch.eq(torch.max(output, 1)[1], y)
+            accuracy = torch.mean(equal.float())
+        ACC += accuracy.item()
+        Write_acc = ACC / (step + 1)
+        Loss += loss.item()
+        Write_Loss = Loss / (step + 1)
+    print(f"Epoch={epoch} | loss={Write_Loss} | lr={optimizer.param_groups[0]['lr']} | Train ACC={Write_acc}")
+
+    # test
+    x = test_x.cuda(0)
+    test_output = net(x)
+    pred_y = torch.max(test_output.to('cpu'), 1)[1].data.numpy()
+    accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
+    print(f'Epoch={epoch} | test ACC: {accuracy}')
+
+    # save train acc and loss in txt
+    trainACCTXT = open("./Train_Acc.txt", 'a')
+    trainACCTXT.write(str(Write_acc))
+    trainACCTXT.write('\n')
+    trainACCTXT.close()
+    trainLossTXT = open("./Train_Loss.txt", 'a')
+    trainLossTXT.write(str(Write_Loss))
+    trainLossTXT.write('\n')
+    trainLossTXT.close()
+    # save test acc
+    trainACCTXT = open("./Test_Acc.txt", 'a')
+    trainACCTXT.write(str(accuracy))
+    trainACCTXT.write('\n')
+    trainACCTXT.close()
+
+    # save model
+    # if Write_acc == 1.0:
+    #     torch.save(net, 'net.pkl')   # both save net and params
+    #     torch.save(net.state_dict(), 'net_params.pkl')   # only save params
+    #     Freeze = True
+    lr_scheduler.step()
+
+
+
+
+
+
---
+++ Readme.txt
@@ -0,0 +1,11 @@
+There are 7 files in this folder:
+
+1.CNN.py :the CNN for image classification on the MNIST dataset(kernel size = 5)
+2.net_5.pkl :the model that have net and parameters of CNN.py (train_acc=1,test_acc=0.9917)
+3.net_params_5.pkl :the ,model only have parameters of CNN.py(train_acc=1,test_acc=0.9917)
+
+4.CNN.py :the CNN for image classification on the MNIST dataset(kernel size = 3)
+5.net_3.pkl :the model that have net and parameters of CNN3.py (train_acc=1,test_acc=0.9923)
+6.net_params_3.pkl :the ,model only have parameters of CNN3.py(train_acc=1,test_acc=0.9923)
+
+7.ZjZhao.yaml :the environment of python(copy my environment with conda:"conda env create -f ZjZhao.yaml")
